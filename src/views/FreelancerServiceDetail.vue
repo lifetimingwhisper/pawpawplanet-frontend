633cacb57e3710f31a43c52b30b1b8bcac44b54e
Emery <iing7050737@gmail.com>
2025-06-21 15:58:25 +0800

    modify: freelancer z-index

--- src/views/FreelancerServiceDetail.vue
+++ src/views/FreelancerServiceDetail.vue
@@ -420,7 +420,7 @@ onMounted(async () => {
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 999;
+    z-index: 1001;
     width: 100%;
     height: 100%;
     background: rgb(0 0 0 / 10%);
