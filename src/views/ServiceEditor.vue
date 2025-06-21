52e851be1a557941a7da981ce7a94f65c4f387a8
Emery <iing7050737@gmail.com>
2025-06-21 15:58:25 +0800

    modify: modify freelancer review img

--- src/views/ServiceEditor.vue
+++ src/views/ServiceEditor.vue
@@ -433,7 +433,7 @@ const getImageUrls = (fileList = []) => {
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 999;
+    z-index: 1001;
     width: 100%;
     height: 100%;
     background: rgb(0 0 0 / 10%);
